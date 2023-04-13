1011a2efb82c679876f7e32433bfe617a3215e93
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-04-13 15:02:33 +0300

    Add caesarChiper, arrayAnalyzer tests and functions

--- src/app.js
+++ src/app.js
@@ -19,4 +19,93 @@ function calculator() {
   };
 }
 
-export { capitalize, reverseString, calculator };
+function caesarCipher(str, shift) {
+  if (str === "") return "";
+  if (shift < 0) return "Shift needs to be positive!";
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  const alphabetIndex = alphabet.reduce((acc, letter, index) => {
+    acc[letter.toLowerCase()] = index + 1;
+    return acc;
+  }, {});
+
+  let convertedStr = "";
+
+  // helper function to check if char is a letter
+  function isLetter(char) {
+    if (char.length === 1 && char.match(/[A-Z|a-z|ü|é]/i)) {
+      return true;
+    }
+    return false;
+  }
+
+  // helper function to check letter case
+  function isUpper(letter) {
+    if (letter.toUpperCase() === letter) {
+      return true;
+    }
+    return false;
+  }
+
+  // helper function to fetch shifted letter, preserving case
+  function shiftLetter(letter, offset) {
+    const letterIndex = alphabetIndex[letter.toLowerCase()];
+    const newLetterIndex = (letterIndex + offset) % 26;
+
+    const newLetter = Object.keys(alphabetIndex).find(
+      (key) => alphabetIndex[key] === newLetterIndex
+    );
+
+    return isUpper(letter) ? newLetter.toUpperCase() : newLetter;
+  }
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < str.length; i++) {
+    if (isLetter(str[i])) {
+      // TODO - shift the letter....
+      convertedStr += shiftLetter(str[i], shift);
+    } else {
+      convertedStr += str[i];
+    }
+  }
+
+  return convertedStr;
+}
+
+function analyzeArray(arr) {
+  return {
+    average: arr.reduce((sum, val) => sum + val, 0) / arr.length,
+    length: arr.length,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+  };
+}
+
+export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
--- src/app.test.js
+++ src/app.test.js
@@ -1,4 +1,10 @@
-import { capitalize, reverseString, calculator } from "./app";
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} from "./app";
 
 test("first letter capitalized", () => {
   expect(capitalize("string")).toBe("String");
@@ -23,5 +29,49 @@ describe("calculator", () => {
   });
   test("divide", () => {
     expect(divide(4, 2)).toBe(2);
+  });
+});
+
+describe("cipher", () => {
+  test("empty string", () => {
+    expect(caesarCipher("", 1)).toBe("");
+  });
+
+  test("negative shift", () => {
+    expect(caesarCipher("abc", -1)).toBe("Shift needs to be positive!");
+  });
+
+  test("base case", () => {
+    expect(caesarCipher("abc", 1)).toBe("bcd");
+  });
+
+  test("punctuation", () => {
+    expect(caesarCipher("a,b", 1)).toBe("b,c");
+  });
+
+  test("keeping the same case", () => {
+    expect(caesarCipher("aB", 1)).toBe("bC");
+  });
+
+  test("wrapping from z to a", () => {
+    expect(caesarCipher("z", 1)).toBe("a");
+  });
+});
+
+describe("analyze array", () => {
+  test("average", () => {
+    expect(analyzeArray([1, 2, 3])).toHaveProperty("average", 2);
+  });
+
+  test("min", () => {
+    expect(analyzeArray([1, 2, 3])).toHaveProperty("min", 1);
+  });
+
+  test("max", () => {
+    expect(analyzeArray([1, 2, 3])).toHaveProperty("max", 3);
+  });
+
+  test("lenght", () => {
+    expect(analyzeArray([1, 2, 3])).toHaveProperty("length", 3);
   });
 });
